fa7d366cfc320c0594496b3840e92a6ef4e1a7e4
ankicha <kicha.antonina@gmail.com>
2024-11-17 13:42:59 +0100

    [FEATURE] Created front project for Swift crud app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {UserTableComponent} from './components/user-table/user-table.component';
+import {AuthenticationModuleComponent} from './components/login-form/authentication-module.component';
+import {NewUserModuleComponent} from './components/user-form/new-user-module.component';
+import {AuthGuardService} from './services/auth-guard-service/auth-guard.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'users', component: UserTableComponent, canActivate: [AuthGuardService]},
+  {path: 'users/new', component: NewUserModuleComponent, canActivate: [AuthGuardService]},
+  {path: 'login', component: AuthenticationModuleComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+
+];
